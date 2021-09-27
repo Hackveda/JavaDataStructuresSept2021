60d3773d4541d9fb02c765c9404ca92ff5acf484
Hackveda <admin@hackveda.in>
2021-09-27 09:49:53 +0530

    Trees - Lesson 9

---
+++ Trees/src/NodeTest.java
@@ -0,0 +1,28 @@
+
+public class NodeTest {
+	
+	public static void main(String[] args) {
+		
+		Node node = new Node(5);
+		node.left = new Node(4);
+		node.right = new Node(8);
+		
+	}
+
+	static class Node {
+		
+		int value; // The value or data to be stored in the Node
+		
+		Node left, right; // Pointers to the left and the right node
+		
+		Node(int value){
+			this.value = value;
+			
+			left = null;
+			right = null;
+			
+		}
+		
+	}
+	
+}
